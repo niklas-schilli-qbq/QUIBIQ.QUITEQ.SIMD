9c5919d6d60e7971526fcccbb2761ec19284c686
Niklas Schilli <a232576@dekra.org>
2025-02-01 18:37:15 +0100

    Formatting, namespace format

--- QUIBIQ.QUIFLOW.SIMD.Benchmarks/GreaterThan.cs
+++ QUIBIQ.QUIFLOW.SIMD.Benchmarks/GreaterThan.cs
@@ -1,67 +1,63 @@
 ﻿using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
 
-namespace QUIBIQ.QUIFLOW.SIMD.Benchmarks
+namespace QUIBIQ.QUIFLOW.SIMD.Benchmarks;
+
+public class GreaterThan
 {
-    public class GreaterThan
+    public static int GreaterThanNaive(int value, ReadOnlySpan<int> array)
     {
-
-        public static int GreaterThanNaive(int value, ReadOnlySpan<int> array)
+        int ct = 0; ;
+        for (int i = 0; i < array.Length; i++)
         {
-            int ct = 0; ;
-            for (int i = 0; i < array.Length; i++)
+            if (array[i] > value)
             {
-                if (array[i] > value)
-                {
-                    ct++;
-                }
+                ct++;
             }
-            return ct;
+        }
+        return ct;
+    }
+
+    public static int GreaterThanSimd(int boundary, ReadOnlySpan<int> array)
+    {
+        var total = 0;
+        var i = 0;
+
+        if (Vector256.IsHardwareAccelerated && array.Length >= Vector256<int>.Count)
+        {
+            var xmm1 = Vector256.Create(boundary);
+            var count = Vector256<int>.Zero;
+            foreach (var vector in MemoryMarshal.Cast<int, Vector256<int>>(array))
+            {
+                count -= Vector256.GreaterThan(vector, xmm1);
+                i += Vector256<int>.Count;
+            }
+
+            total = Vector256.Sum(count);
         }
 
+        array = array[i..];
+        i = 0;
 
-        public static int GreaterThanSimd(int boundary, ReadOnlySpan<int> array)
+        if (Vector128.IsHardwareAccelerated && array.Length >= Vector128<int>.Count)
         {
-            var total = 0;
-            var i = 0;
-
-            if (Vector256.IsHardwareAccelerated && array.Length >= Vector256<int>.Count)
+            var xmm1 = Vector128.Create(boundary);
+            var count = Vector128<int>.Zero;
+            foreach (var vector in MemoryMarshal.Cast<int, Vector128<int>>(array))
             {
-                var xmm1 = Vector256.Create(boundary);
-                var count = Vector256<int>.Zero;
-                foreach (var vector in MemoryMarshal.Cast<int, Vector256<int>>(array))
-                {
-                    count -= Vector256.GreaterThan(vector, xmm1);
-                    i += Vector256<int>.Count;
-                }
-
-                total = Vector256.Sum(count);
+                count -= Vector128.GreaterThan(vector, xmm1);
+                i += Vector128<int>.Count;
             }
 
-            array = array[i..];
-            i = 0;
-
-            if (Vector128.IsHardwareAccelerated && array.Length >= Vector128<int>.Count)
-            {
-                var xmm1 = Vector128.Create(boundary);
-                var count = Vector128<int>.Zero;
-                foreach (var vector in MemoryMarshal.Cast<int, Vector128<int>>(array))
-                {
-                    count -= Vector128.GreaterThan(vector, xmm1);
-                    i += Vector128<int>.Count;
-                }
-
-                total += Vector128.Sum(count);
-            }
-
-            for (; i < array.Length; i++)
-            {
-                if (array[i] > boundary)
-                    total++;
-            }
-
-            return total;
+            total += Vector128.Sum(count);
         }
 
+        for (; i < array.Length; i++)
+        {
+            if (array[i] > boundary)
+                total++;
+        }
+
+        return total;
     }
 }
--- QUIBIQ.QUIFLOW.SIMD.Benchmarks/GreaterThanBenchmark.cs
+++ QUIBIQ.QUIFLOW.SIMD.Benchmarks/GreaterThanBenchmark.cs
@@ -1,33 +1,26 @@
 ﻿using BenchmarkDotNet.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace QUIBIQ.QUIFLOW.SIMD.Benchmarks
+namespace QUIBIQ.QUIFLOW.SIMD.Benchmarks;
+
+public class GreaterThanBenchmark_RandomValues
 {
-    public class GreaterThanBenchmark_RandomValues
+    private int[] data;
+    [Params(1024, 8192)]
+    public int N;
+
+    [GlobalSetup]
+    public void Setup()
     {
-        private int[] data;
-        [Params(1024, 8192)]
-        public int N;
-
-        [GlobalSetup]
-        public void Setup()
+        data = new int[N];
+        for(int i = 0; i < N; i++)
         {
-            data = new int[N];
-            for(int i = 0; i < N; i++)
-            {
-                data[i] = i;
-            }
+            data[i] = i;
         }
-        [Benchmark]
-        public int GreaterThanNaive() => GreaterThan.GreaterThanNaive( N/2, new ReadOnlySpan<int>(data));
-
-        [Benchmark]
-        public int GreaterThanSimd() => GreaterThan.GreaterThanSimd(N / 2, new ReadOnlySpan<int>(data));
     }
+
+    [Benchmark]
+    public int GreaterThanNaive() => GreaterThan.GreaterThanNaive( N/2, new ReadOnlySpan<int>(data));
+
+    [Benchmark]
+    public int GreaterThanSimd() => GreaterThan.GreaterThanSimd(N / 2, new ReadOnlySpan<int>(data));
 }
